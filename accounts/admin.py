67a808dfa2be18d22a7fe44e75c70d2087d893b5
MaxFine <gibboel5@gmail.com>
2023-08-16 15:19:01 +0000

    auth configs

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CustomUser
+
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'is_active', 'is_staff']
+    search_fields = ['username', 'email']
+    list_filter = ['email']
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+
+    username = models.CharField(label='Username',  max_length=20, unique=True)
+    email = models.EmailField(label='Email Address', unique=True)
+    
+    USERNAME_FIELD = 'username' 
+    REQUIRED_FIELDS = ['email']  
+
+    def __str__(self):
+        return self.username
+    
